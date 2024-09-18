f2a9533d157c0277c5df1497b4d6624663b11af1
vinegraft <982042+vinegraft@users.noreply.github.com>
2024-09-18 12:05:08 -0400

    Add logic to make canvas adapt to window size

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
-const container = document.querySelector(".container");
-container.textContent = "test";
+let container = document.querySelector(".container");
+
+resizeCanvas();
+
+window.addEventListener("resize", resizeCanvas);
+
+function resizeCanvas() {
+  let windowHeight = window.innerHeight;
+  let windowWidth = window.innerWidth;
+
+  // Make canvas a square constrained by smallest window dimension
+  if (windowHeight >= windowWidth) {
+    container.style.width = `${windowWidth * 0.9}px`;
+    container.style.height = `${windowWidth * 0.9}px`;
+  } else {
+    container.style.width = `${windowHeight * 0.9}px`;
+    container.style.height = `${windowHeight * 0.9}px`;
+  }
+}
