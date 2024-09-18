5f64ecac7dbe98b7e94767cf0c42ba0f37bc9ed3
vinegraft <982042+vinegraft@users.noreply.github.com>
2024-09-18 14:51:31 -0400

    Update canvas sizing logic
    Removes deprecated .style method

--- script.js
+++ script.js
@@ -6,17 +6,15 @@ createGrid(gridSize);
 
 window.addEventListener("resize", resizeCanvas);
 
+// Make canvas a square constrained by smallest window dimension
 function resizeCanvas() {
   let windowHeight = window.innerHeight;
   let windowWidth = window.innerWidth;
 
-  // Make canvas a square constrained by smallest window dimension
   if (windowHeight >= windowWidth) {
-    container.style.width = `${windowWidth * 0.9}px`;
-    container.style.height = `${windowWidth * 0.9}px`;
+    container.setAttribute("style", `width: ${windowWidth * 0.9}px; height: ${windowWidth * 0.9}px;`)
   } else {
-    container.style.width = `${windowHeight * 0.9}px`;
-    container.style.height = `${windowHeight * 0.9}px`;
+    container.setAttribute("style", `width: ${windowHeight * 0.9}px; height: ${windowHeight * 0.9}px;`)
   }
 }
 
